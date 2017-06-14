120f09384b24759d9c9e1241d8b853c93678906e
pengjiemail <pengjiemail@163.com>
2017-06-14 20:06:58 +0800

    aaaa

---
+++ src/te/url/ApacheUrl.java
@@ -0,0 +1,22 @@
+/**
+ * File：ApacheUrl.java
+ * Package：te.url
+ * Author：pengjie
+ * Date：2017年6月12日 下午8:53:50
+ * Copyright (C) 2003-2017 搜房资讯有限公司-版权所有
+ */
+package te.url;
+
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+
+/**
+ * @author pengjie
+ */
+public class ApacheUrl {
+  
+  public static void main(String[] args) {
+    HttpClient hc = new HttpClient();
+    PostMethod pm = new PostMethod("");
+  }
+}
